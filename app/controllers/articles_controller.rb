da594a621e4ba58724e81740f90200adecf3de0b
charlottedibb <charlotte.dibb@gmail.com>
2022-10-16 23:17:01 -0700

    Show, edit, and destroy articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,10 @@ class ArticlesController < ApplicationController
     render :index
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
@@ -14,12 +18,31 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to articles_url
     else
-      redirect_back(fallback_location: root_path)
+      render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to articles_url
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_url
+  end
+
   private
-  
+
   def article_params
     params.require(:article).permit(:title, :body)
   end 
