5bdbea325be8f22d09ba82e60849f44e6ae2507f
charlottedibb <charlotte.dibb@gmail.com>
2022-11-01 19:51:51 -0700

    Rewrite model tests with factory

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,22 +1,29 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe Article, type: :model do
+  it "has a valid factory" do
+    expect(FactoryBot.build(:article)).to be_valid
+  end
   it "is valid with all of it's parameters" do
-    article = Article.new(title: 'Hello', body: 'World', author: 'Daniel Ricciardo')
+    article = Article.new(title: "Hello", body: "World", author: "Daniel Ricciardo")
+    expect(article).to be_valid
+  end
+  it "is valid with factory parameters" do
+    article = FactoryBot.build(:article)
     expect(article).to be_valid
   end
   it "is valid with only required parameters" do
-    article = Article.new(title: 'Hello', body: 'World')
+    article = FactoryBot.build(:article, author: nil)
     expect(article).to be_valid
   end
   it "is invalid without required parameters" do
-    article = Article.new(title: 'Hello')
+    article = FactoryBot.build(:article, body: nil)
     expect(article).to_not be_valid
   end
 
-  it "parameter is unique" do
-    article = Article.create(title: 'Hello', body: 'World')
-    article2 = Article.create(title: 'Hello', body: 'World')
+  it "is invalid without title parameter s" do
+    article = FactoryBot.create(:article, title: "Hello")
+    article2 = FactoryBot.build(:article, title: "Hello")
     article2.valid?
 
     expect(article2).to_not be_valid
